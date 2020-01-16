ef7c4f4c1ce4f265d66afaa349950d86f2b0eb47
kyeongmin <ekmst1027@gmail.com>
2020-01-17 00:23:02 +0900

    JPA 시작하기

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -2,7 +2,9 @@ package hellojpa;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
@@ -11,10 +13,36 @@ public class JpaMain {
                 Persistence.createEntityManagerFactory("hello");
 
         EntityManager em = emf.createEntityManager();
-        // code
 
-        em.close();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
 
+        try {
+            // Insert Query
+//            Member member = new Member();
+//            member.setId(1L);
+//            member.setName("HelloA");
+//            em.persist(member);
+
+            // Update Query
+//            Member findMember = em.find(Member.class, 1L);
+//            findMember.setName("HelloJPA");
+
+            // JPQL 예제
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .getResultList();
+
+            for (Member member : result) {
+                System.out.println("member.name = " + member.getName());
+            }
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
         emf.close();
+
     }
 }
